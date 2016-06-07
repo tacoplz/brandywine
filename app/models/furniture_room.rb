de42a8b2c642d2eab63c6d009f6fc5ba3d11fcf4
tacoplz <punkrox_08@hotmail.com>
2016-06-07 13:43:03 -0600

    Updating models and forms

--- app/models/furniture_room.rb
+++ app/models/furniture_room.rb
@@ -1,2 +1,7 @@
 class FurnitureRoom < ActiveRecord::Base
+
+  def self.roomtypes
+    FurnitureRoom.all.map { |room| room.room_type}
+  end
+
 end
--- app/models/furniture_type.rb
+++ app/models/furniture_type.rb
@@ -1,2 +1,11 @@
 class FurnitureType < ActiveRecord::Base
+
+  def self.roomtypes
+    FurnitureRoom.all.map {|room| room.room_type}
+  end
+
+  def self.furnituretypes
+    FurnitureType.all.map { |type| type.furniture_class}
+  end
+
 end
