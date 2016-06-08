e78a030a2cd410f828ef266f399a2245c4a5a848
tacoplz <punkrox_08@hotmail.com>
2016-06-07 18:29:33 -0600

    working on the form

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  function showForm() {
+    var selroom = document.getElementById(room_for_furniture).value;
+    if (selroom == 'Bedroom') {
+      /*document.getElemenById(room_for_furniture).style.display = "block";*/
+      document.getElementById(furniture_type).style.display = "block";
+    }
+  }
+</script>
