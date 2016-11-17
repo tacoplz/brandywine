b5d6ec30e23dbc49248305e49d218a4f2ca57739
tacoplz <punkrox_08@hotmail.com>
2016-11-16 21:25:02 -0700

    config. mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  host = 'ec2-54-201-124-144.us-west-2.compute.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'email-smtp.us-west-2.amazonaws.com',
     :port           => '587',
