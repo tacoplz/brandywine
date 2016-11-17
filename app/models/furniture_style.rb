41d31a7a03aa48f9d8ebb6d189e735bc09a0e93c
tacoplz <punkrox_08@hotmail.com>
2016-11-16 18:24:21 -0700

    mission seed

--- app/models/furniture_style.rb
+++ app/models/furniture_style.rb
@@ -3,7 +3,7 @@ class FurnitureStyle < ActiveRecord::Base
   #relys on the paperclip gem
   validates :furniture_style,  presence: true, format: { with: /\A[\s\w&%()]*\z/ }, length: { maximum: 50 }
 
-  has_attached_file :furniture_style_image, styles: { small: "150x150#" }
+  has_attached_file :furniture_style_image, styles: { small: "150x150>" }
   validates_attachment :furniture_style_image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
   validates_attachment_file_name :furniture_style_image, matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]
   validates_with AttachmentSizeValidator, attributes: :furniture_style_image, less_than: 200.kilobytes, :unless => Proc.new {|m| m[:furniture_style_image].nil?}
