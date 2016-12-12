5f8467ad843c836be754af21b3a537aed02881fa
tacoplz <punkrox_08@hotmail.com>
2016-12-11 20:27:20 -0700

    Default Email

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ApplicationMailer
-  default from: "brandywinefurniture@yahoo.com"
+  default from: "no-reply@thebrandywinefurniture.net"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
